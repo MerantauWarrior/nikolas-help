20033eef4fe2abab7900386a68301604467f46d7
boriskleshch <b.kleshch@bsg.world>
2022-05-16 00:13:06 +0300

    2 sections +

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,50 @@
 $( document ).ready(function() {
   console.log('asd')
 
+  $('.stream-slider').slick({
+    dots: false,
+    infinite: false,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+  $('.faqs-item__head').click(function () {
+    $(this).parent().toggleClass('faqs-item--active');
+    $(this).siblings('.faqs-item__body').slideToggle(250);
+
+  });
+
+  $('.work-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 250,
+    slidesToShow: 1,
+    variableHeight: true,
+    prevArrow: $('.work-slider__prev'),
+    nextArrow: $('.work-slider__next')
+  });
+
   $('.projects-slider').slick({
     dots: false,
     infinite: false,
     speed: 250,
     slidesToShow: 1,
-    variableWidth: true,
+    variableHeight: true,
     prevArrow: $('.projects-slider__prev'),
     nextArrow: $('.projects-slider__next')
   });
@@ -14,6 +52,23 @@ $( document ).ready(function() {
   $('.projects-tab__title').click(function () {
     $('.projects-tab').removeClass('projects-tab--active');
     $(this).parent().addClass('projects-tab--active');
+  });
+
+  $('.js-copy').click(function () {
+    let value = $(this).data('value');
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText){
+        return navigator.clipboard.writeText(value);
+    }else{
+      const el = document.createElement('textarea');
+      el.value = value;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    }
   });
 
 });
